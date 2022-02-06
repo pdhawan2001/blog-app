c30e42f40eba0a1d77b79b58ee0797549ca8bc14
Parth Dhawan <pdhawan2001@gmail.com>
2022-02-07 02:16:52 +0530

    added moderation service

--- client/src/CommentList.js
+++ client/src/CommentList.js
@@ -1,22 +1,21 @@
+/* eslint-disable react/prop-types */
 /* eslint-disable react/display-name */
-import React, { useState, useEffect } from 'react';
-import axios from 'axios';
+import React /* { useState, useEffect } */ from 'react';
+// import axios from 'axios';
 
-// eslint-disable-next-line react/prop-types
-export default ({ postId }) => {
-  const [comments, setComments] = useState([]);
+export default ({ comments }) => {
+  // const [comments, setComments] = useState([]);
 
-  const fetchData = async () => {
-    const res = await axios.get(
-      `http://localhost:4001/posts/${postId}/comments`
-    );
-    setComments(res.data);
-  };
+  // const fetchData = async () => {
+  //   const res = await axios.get(
+  //     `http://localhost:4001/posts/${postId}/comments`
+  //   );
+  //   setComments(res.data);
+  // };
 
-  useEffect(() => {
-    fetchData();
-  }, []);
-
+  // useEffect(() => {
+  //   fetchData();
+  // }, []);
   const renderedComments = comments.map((comment) => {
     return <li key={comment.id}>{comment.content}</li>;
   });
